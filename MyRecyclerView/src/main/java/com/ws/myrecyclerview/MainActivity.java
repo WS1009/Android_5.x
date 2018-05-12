d2c0431f7a6fa70af5efd7fa3d42a5dee6d7fec9
WS1009 <WANGSHUN611@163.com>
2018-05-12 11:10:30 +0800

    test

--- MyRecyclerView/src/main/java/com/ws/myrecyclerview/MainActivity.java
+++ MyRecyclerView/src/main/java/com/ws/myrecyclerview/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        //test
+        //test content view
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
